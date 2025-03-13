a6eeff9aa26e15d23c86445ed0b20012d32e6a2c
Rishi venkata kousik <123746270+Rishivenkatakousik@users.noreply.github.com>
2025-03-14 00:46:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
 import Hero from "./components/custom/Hero";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch("https://travel-hs2t.onrender.com/track-visit")
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
   return (
     <div>
       <Hero />
